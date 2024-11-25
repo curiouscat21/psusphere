d06e296fd0a5ef958bc92421f9c7cd48bfc8f46c
curiouscat21 <202280185@psu.palawan.edu.ph>
2024-11-25 09:02:43 +0800

    completed views

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
+from studentorg.views import (HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, 
+                              OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView, CollegeList, CollegeCreateView, 
+                              CollegeUpdateView, CollegeDeleteView, ProgramList, ProgramCreateView, ProgramUpdateView, ProgramDeleteView, StudentList, 
+                              StudentCreateView, StudentUpdateView, StudentDeleteView)
 from studentorg import views
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -30,6 +34,26 @@ urlpatterns = [
     re_path(r'^login/$', auth_views.LoginView.as_view(
         template_name='login.html'), name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    # org member urls 
+    path('orgmember_list', OrgMemberList.as_view(), name='orgmember-list'),
+    path('orgmember_list/add', OrgMemberCreateView.as_view(), name='orgmember-add'),
+    path('orgmember_list/<pk>', OrgMemberUpdateView.as_view(), name='orgmember-update'),
+    path('orgmember_list/<pk>/delete', OrgMemberDeleteView.as_view(), name='orgmember-delete'),
+    # student urls 
+    path('student_list', StudentList.as_view(), name='student-list'),
+    path('student_list/add', StudentCreateView.as_view(), name='student-add'),
+    path('student_list/<pk>', StudentUpdateView.as_view(), name='student-update'),
+    path('student_list/<pk>/delete', StudentDeleteView.as_view(), name='student-delete'),
+    # college urls 
+    path('college_list', CollegeList.as_view(), name='college-list'),
+    path('college_list/add', CollegeCreateView.as_view(), name='college-add'),
+    path('college_list/<pk>', CollegeUpdateView.as_view(), name='college-update'),
+    path('college_list/<pk>/delete', CollegeDeleteView.as_view(), name='college-delete'),
+    # program urls 
+    path('program_list', ProgramList.as_view(), name='program-list'),
+    path('program_list/add', ProgramCreateView.as_view(), name='program-add'),
+    path('program_list/<pk>', ProgramUpdateView.as_view(), name='program-update'),
+    path('program_list/<pk>/delete', ProgramDeleteView.as_view(), name='program-delete'),
 
 ]
 
--- projectsite/studentorg/forms.py
+++ projectsite/studentorg/forms.py
@@ -1,8 +1,37 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Organization
+from .models import OrgMember
+from .models import Student
+from .models import College
+from .models import Program
 
 class OrganizationForm(ModelForm):
     class Meta:
         model = Organization
         fields = "__all__"
+
+class OrgMemberForm(forms.ModelForm):
+    class Meta:
+        model = OrgMember
+        fields = ['student', 'organization', 'date_joined']
+        widgets = {
+            'date_joined': forms.DateInput(attrs={'type': 'date'})
+        }
+
+class StudentForm(ModelForm):
+    class Meta:
+        model = Student
+        fields = "__all__" 
+
+class CollegeForm(ModelForm):
+    class Meta:
+        model = College
+        fields = "__all__" 
+        
+
+class ProgramForm(ModelForm):
+    class Meta:
+        model = Program
+        fields = "__all__" 
+
